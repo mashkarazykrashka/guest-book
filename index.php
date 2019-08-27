52b3be63d9a64819432c404c73956c0650bcb98e
mashkarazykrashka <mashkarazykrashka@gmail.com>
2019-08-27 19:41:37 +0300

    for

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -7,8 +8,28 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
 </head>
+
 <body>
     Hello world!
     New hello
+
+    <?php
+    $i = 0;
+    while ($i++ < 10) echo $i;
+    echo "<br>";
+
+    for ($i = 0; $i < 10; $i++) echo $i;
+    echo "<br>";
+
+    $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    foreach ($arr as $value) {
+        echo $value;
+    }
+    ?>
+
+
+
 </body>
+
 </html>
