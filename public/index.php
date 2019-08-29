817a6680f60c170a0e2c7d6a6d35d9f4a0e24af6
mashkarazykrashka <mashkarazykrashka@gmail.com>
2019-08-29 19:25:50 +0300

    auto

--- public/index.php
+++ public/index.php
@@ -1,35 +1,18 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Guest book</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
-</head>
+include "../src/autoload.php";
 
-<body>
-    Hello world!
-    New hello
+use App\Core\Router;
 
-    <?php
-    $i = 0;
-    while ($i++ < 10) echo $i;
-    echo "<br>";
+$obj = new Router();
+$obj->run();
 
-    for ($i = 0; $i < 10; $i++) echo $i;
-    echo "<br>";
 
-    $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    foreach ($arr as $value) {
-        echo $value;
-    }
-    ?>
 
-<img src="pic.jpg">
 
-</body>
 
-</html>
+
+
+
+?>
---
+++ src/core/router.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Core;
+
+class Router
+{
+    public function Run()
+    { 
+        echo "Hello world!";
+    }
+}
