506d0b882aa6aee8c9a877072c1d770c609ada91
mashkarazykrashka <mashkarazykrashka@gmail.com>
2019-08-29 20:00:31 +0300

    run

--- src/core/router.php
+++ src/core/router.php
@@ -1,10 +1,30 @@
 <?php
+
 namespace App\Core;
 
 class Router
 {
+    function __construct()
+    {
+        $this->controllerName = ($_GET["t"] ?? 'site') . 'Controller';
+
+        $this->actionName = 'action' . ($_GET["a"] ?? 'home');
+    }
+
     public function Run()
-    { 
-        echo "Hello world!";
+    {
+        $className = "App\\Controller\\{$this->controllerName}";
+
+        if (class_exists($className)) {
+            $MVC = new $className();
+
+            if (method_exists($MVC, $this->actionName)) {
+                $MVC->{$this->actionName}();
+            } else {
+                echo "нет такого метода: $this->actionName";
+            }
+        } else {
+            echo "нет такого класса: $this->controllerName";
+        }
     }
 }
