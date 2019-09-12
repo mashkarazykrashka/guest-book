a7177372a6824aedc9e192a2d0ed41b72c6afc7f
mashkarazykrashka <mashkarazykrashka@gmail.com>
2019-09-12 20:46:31 +0300

    f

--- templates/_layouts/mainLayout.php
+++ templates/_layouts/mainLayout.php
@@ -21,16 +21,6 @@
 
         <div class="collapse navbar-collapse" id="navbar1">
             <ul class="navbar-nav mr-auto">
-                <!-- <li class="nav-item dropdown">
-                    <a class="btn btn-sm btn-outline-info my-2 my-sm-0 dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tables</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                        <a class="dropdown-item" href="?t=tableOne&a=showtable">Ведомость</a>
-                        <a class="dropdown-item" href="?t=tableTwo&a=showtable">Анекдоты</a>
-                    </div>
-                </li> -->
-                <!-- <li class="nav-item active">
-                    <a class="btn btn-sm btn-outline-info my-2 my-sm-0" href="?t=TicTac&a=ShowField" role="button">TicTac<span class="sr-only">(current)</span></a>
-                </li> -->
                 <li class="nav-item">
                 <li class="nav-item active">
                     <a class="btn btn-sm btn-outline-info my-2 my-sm-0" href="?t=FeedBack&a=ShowForm" role="button">FeedBack<span class="sr-only">(current)</span></a>
@@ -53,10 +43,6 @@
                 </li>
 
             </ul>
-            <!-- <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-            </form>  -->
         </div>
     </nav>
     <div class="user_name">
